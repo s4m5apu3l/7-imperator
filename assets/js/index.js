3236e1076861dfd605d7f5800a0978bb7ff8e36c
beachislove <asperatuz@yandex.ru>
2023-03-01 22:47:17 +0600

    feat: add gsap and l-profit section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,26 @@
-console.log("ontouchstart" in document.documentElement)
-
-const parallax = document.querySelector('.l-hero--img img');
-const container = document.querySelector('.l-hero__left');
+// on hero section mouse parallax
+const parallax = document.querySelector(".l-hero--img img");
+const container = document.querySelector(".l-hero__left");
 
 if (!("ontouchstart" in document.documentElement)) {
-  container.addEventListener('mousemove', function(event) {
+  container.addEventListener("mousemove", function (event) {
     const x = event.clientX / container.offsetWidth;
     const y = event.clientY / container.offsetHeight;
 
     parallax.style.transform = `translate(-${x * 10}px, -${y * 10}px)`;
   });
 }
+
+// gsap scroll
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: ".smooth-wrapper",
+    content: ".smooth-content",
+    smooth: 1.5,
+    effects: true,
+  });
+}
+
+
