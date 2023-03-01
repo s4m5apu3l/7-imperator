ddefd4b6f3f2954bdb17348c860e31bd12bd19a7
beachislove <asperatuz@yandex.ru>
2023-03-01 23:43:48 +0600

    feat: add l-profit-section delete for a few time js code main banner

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
 // on hero section mouse parallax
-const parallax = document.querySelector(".l-hero--img img");
-const container = document.querySelector(".l-hero__left");
+// const parallax = document.querySelector(".l-hero--img img");
+// const container = document.querySelector(".l-hero__left");
 
-if (!("ontouchstart" in document.documentElement)) {
-  container.addEventListener("mousemove", function (event) {
-    const x = event.clientX / container.offsetWidth;
-    const y = event.clientY / container.offsetHeight;
+// if (!("ontouchstart" in document.documentElement)) {
+//   container.addEventListener("mousemove", function (event) {
+//     const x = event.clientX / container.offsetWidth;
+//     const y = event.clientY / container.offsetHeight;
 
-    parallax.style.transform = `translate(-${x * 10}px, -${y * 10}px)`;
-  });
-}
+//     parallax.style.transform = `translate(-${x * 10}px, -${y * 10}px)`;
+//   });
+// }
 
 // gsap scroll
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
